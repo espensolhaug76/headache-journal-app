57f313699c293c3bf7fd429d3805dc5042c96da4
espensolhaug76 <153593072+espensolhaug76@users.noreply.github.com>
2025-05-23 18:13:08 +0200

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   Line,
   XAxis,
@@ -7,7 +7,6 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
-  BarChart,
   Bar,
   ComposedChart
 } from 'recharts';
